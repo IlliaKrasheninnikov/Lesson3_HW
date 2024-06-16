6e22b992a793bd12ab3245a16585c84a47571026
IlliaKrasheninnikov <krasheninnikovillia@gmail.com>
2024-06-16 19:58:02 +0200

    MyWhere added

--- Lesson3_HW/Program.cs
+++ Lesson3_HW/Program.cs
@@ -9,12 +9,22 @@ class Program
 {
     private static void Main(string[] args)
     {
-        User user1 = new User(19);
-        UserInput("Enter your gender Male (or 0), Female (or 1), or Mechanic (or 2). :", input => user1.Gender = input);
-        UserInput("Enter your first name:", input => user1.FirstName = input);
-        UserInput("Enter your second name:", input => user1.SecondName = input);
+        //User user1 = new User(19);
+        //UserInput("Enter your gender Male (or 0), Female (or 1), or Mechanic (or 2). :", input => user1.Gender = input);
+        //UserInput("Enter your first name:", input => user1.FirstName = input);
+        //UserInput("Enter your second name:", input => user1.SecondName = input);
 
-        Console.WriteLine(user1);
+        //Console.WriteLine(user1);
+
+        var list = new List<int> {1, 2, 3, 4 };
+        var oddQuery = list.MyWhere(i => i % 2 != 0);
+        list.Add(5);
+        var filterredList = oddQuery.ToList();
+        foreach (var item in filterredList) 
+        {
+            Console.WriteLine(item.ToString());
+        }
+
 
     }
     public static void UserInput(string Message, Action<string> setInput)
